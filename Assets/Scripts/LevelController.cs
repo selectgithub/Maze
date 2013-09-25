59b6d2b07fb5aaaf757bd1943b4245ceb9eabd35
Chris <cwang@neurosky.com>
2013-09-25 15:50:57 +0800

    init project

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider c){
+		Application.LoadLevel(Application.loadedLevel+1);
+	}
+}
---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereController : MonoBehaviour {
+	public float speed = 1.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.W)){
+			transform.Translate(Vector3.forward*Time.deltaTime*speed);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.S)){
+			transform.Translate(Vector3.back*Time.deltaTime*speed);
+		}
+		if(Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D)){
+			transform.Translate(Vector3.right*Time.deltaTime*speed);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A)){
+			transform.Translate(Vector3.left*Time.deltaTime*speed);
+		}
+	}
+}
