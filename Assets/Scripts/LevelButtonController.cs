4812606948cc18d1b58046fac197623174881dfe
Chris <cwang@neurosky.com>
2013-09-29 11:04:50 +0800

    Added Menu Scene and Home Button in Level Scene

---
+++ Assets/Scripts/LevelButtonController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnHomeButtonClicked(){
+		Application.LoadLevel("Menu");
+	}
+}
---
+++ Assets/Scripts/MenuScripts/MenuUIController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuUIController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnLevel1Clicked(){
+		Application.LoadLevel("C");
+	}
+	void OnLevel2Clicked(){
+		Application.LoadLevel("D");
+	}
+	void OnLevel3Clicked(){
+		Application.LoadLevel("E");
+	}
+}
